ccca34e9a19f8c19342d22711ef9843d199bed18
Regina Annemarie Bool <ninisalter@gmail.com>
2026-02-10 16:04:19 +0800

    Initial commit of Item 2

---
+++ Regina Annemarie I. Bool/Practice Activity/CurrencyConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class CurrencyConverter
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter amount in USD: ");
+        double amt = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter exchange rate from USD to EUR: ");
+        double rate = double.Parse(Console.ReadLine());
+
+        double ans = amt * rate;
+
+        Console.Write($"Amount in EUR: {ans:F2}");
+    }
+}
