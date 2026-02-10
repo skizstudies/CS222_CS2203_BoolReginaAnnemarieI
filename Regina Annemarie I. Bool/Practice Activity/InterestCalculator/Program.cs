193951485c200b1ea76a45819ce3dab14223cac0
Regina Annemarie Bool <ninisalter@gmail.com>
2026-02-10 15:52:58 +0800

    Initial commit

---
+++ Regina Annemarie I. Bool/Practice Activity/InterestCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class InterestCalculator
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter principal amount: ");
+        double principal = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter rate of interest: ");
+        double rate = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter time in years: ");
+        int time = int.Parse(Console.ReadLine());
+
+        double si = (principal * rate * time) / 100;
+        Console.WriteLine($"Simple Interest: {si}");
+    }
+}
